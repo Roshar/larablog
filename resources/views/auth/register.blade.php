158afc0647257ad43f42665c0078c1a6d1a305c2
Roshah <rashid.batukaev@gmail.com>
2018-08-09 22:40:19 +0300

    afterModification

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Регистрация пользователя </title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+    <script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/E3496CBE-2723-7E48-8114-0647ABF3A05C/main.js" charset="UTF-8"></script></head>
+
+<body>
+
+<div class="container">
+
+    <form class="form-signin">
+        <h2 class="form-signin-heading">Зарегистрироваться</h2>
+        <label for="inputEmail" class="sr-only">Email адрес</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email адрес" required autofocus name="mail">
+        <label for="inputPassword" class="sr-only">Пароль</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required name="pass">
+        <label for="inputPasswordSecond" class="sr-only">Пароль</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Повторить пароль" required name="pass2">
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" value="Запомнить меня">Запомнить меня
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Зарегистрироваться </button>
+    </form>
+
+</div> <!-- /container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
